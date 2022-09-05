655b4d99705a53b57384d18d9d2c7f49581e78ab
Denis Rodionov <nicholas.hawkwood@gmail.com>
2022-09-06 01:18:58 +0300

    comments

--- src/handlers/helpers.rs
+++ src/handlers/helpers.rs
@@ -4,17 +4,20 @@ use log::error;
 use serde::Serialize;
 use tera::Context;
 
+/// Returns HTTP response from html string, setting content type and status 200.
 pub fn ok_response(html: String) -> HttpResponse {
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(html)
 }
 
+/// Processes error on controller/handler level.
 pub fn handle_error(err: anyhow::Error) -> Error {
     error!("{}", err);
     error::ErrorInternalServerError("Server error happened")
 }
 
+/// Renders tera html template to string.
 pub fn render_html<T: Serialize>(
     tmpl: &web::Data<tera::Tera>,
     name: &str,
